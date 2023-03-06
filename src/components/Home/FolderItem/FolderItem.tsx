b95d5a6f0504d8f4c9677c2f2e86176d03a34d7b
Aleksey Yesin <esinru@gmail.com>
2023-03-06 04:06:10 +0200

    Add folder expand/collapse

--- src/components/Home/FolderItem/FolderItem.tsx
+++ src/components/Home/FolderItem/FolderItem.tsx
@@ -1,4 +1,6 @@
 import { FC, useMemo } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 
 import { IGetFileTreeFile } from "services/api/types";
 import css from "./FolderItem.module.scss";
@@ -28,6 +30,7 @@ export const FolderItem: FC<IFolderItems> = ({
 }) => {
   const sortField = folderParams?.sortField || defaultFolderParams.sortField;
   const sortIsAsc = folderParams?.sortIsAsc ?? defaultFolderParams.sortIsAsc;
+  const isExpanded = folderParams?.isExpanded ?? defaultFolderParams.isExpanded;
 
   const sortedFiles = useMemo(() => {
     const compareFn = (file1: IGetFileTreeFile, file2: IGetFileTreeFile) => {
@@ -41,6 +44,15 @@ export const FolderItem: FC<IFolderItems> = ({
     };
     return [...files].sort(compareFn);
   }, [sortField, sortIsAsc, files]);
+
+  const toggleIsExpanded = () => {
+    setFolderParams({
+      ...defaultFolderParams,
+      ...(folderParams || {}),
+      name: folderName,
+      isExpanded: !isExpanded,
+    });
+  };
 
   const handleSortClick: THandleSortClick = (field) => {
     const isAsc = field === sortField ? !sortIsAsc : true;
@@ -56,22 +68,33 @@ export const FolderItem: FC<IFolderItems> = ({
 
   return (
     <li>
-      <b className={css.folderName}>{folderName}</b>
-
-      <ul>
-        {!files.length && <li className={css.noItems}>Folder is empty</li>}
-
-        {!!files.length && (
-          <FileHeader
-            sortField={sortField}
-            sortIsAsc={sortIsAsc}
-            onSortClick={handleSortClick}
+      <div className={css.folderHeader}>
+        <button className={css.folderNameBtn} onClick={toggleIsExpanded}>
+          <FontAwesomeIcon
+            icon={faChevronRight}
+            size="sm"
+            rotation={isExpanded ? 90 : undefined}
           />
-        )}
-        {sortedFiles.map((file) => (
-          <FileItem key={file.name} file={file} />
-        ))}
-      </ul>
+          <span>{folderName}</span>
+        </button>
+      </div>
+
+      {isExpanded && (
+        <ul>
+          {!files.length && <li className={css.noItems}>Folder is empty</li>}
+
+          {!!files.length && (
+            <FileHeader
+              sortField={sortField}
+              sortIsAsc={sortIsAsc}
+              onSortClick={handleSortClick}
+            />
+          )}
+          {sortedFiles.map((file) => (
+            <FileItem key={file.name} file={file} />
+          ))}
+        </ul>
+      )}
     </li>
   );
 };
